781a2460bdfb4618993b3e5a76c1348115929978
Darren Potter <darpots@outlook.com>
2022-09-21 17:27:54 +0100

    fix(Svelte config): Remove dev plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	},
-	vitePlugin: {
-		experimental: {
-			inspector: {
-				// change shortcut : string
-				toggleKeyCombo: 'meta-shift',
-				// deactivate after holding : boolean
-				holdMode: true,
-				// when to show the button 'always' | 'active | 'never'
-				showToggleButton: 'active',
-				// where to show the button 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
-				toggleButtonPos: 'top-right'
-			}
-		}
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
